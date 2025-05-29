a14f8b53ef454304c4c72d1f0fbef3a42c6383cd
rubel1893 <122612119+rubel1893@users.noreply.github.com>
2025-05-29 23:14:47 +0600

    Update site-bundle.js

--- scripts/site-bundle.js
+++ scripts/site-bundle.js
@@ -59,3 +59,11 @@ requestIdleCallback(() => {
   script.defer = true;
   document.head.appendChild(script);
 });
+
+(window.requestIdleCallback || function(cb) { setTimeout(cb, 200); })(function () {
+  const script = document.createElement('script');
+  script.src = "https://assets.squarespace.com/universal/scripts-compressed/extract-css-moment-js-vendor-088ec0120fdbcfed-min.en-US.js";
+  script.defer = true;
+  document.head.appendChild(script);
+});
+
